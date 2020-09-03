518587576cfb5eb2a0df55e7401aa313004f647d
MarcioSill <marcio.sill@hotmail.com>
2020-09-02 21:48:35 -0300

    Ajuste

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,24 +8,26 @@ public class Program {
 
 	public static void main(String[] args) {
 
-try {
-	
-	String name = JOptionPane.showInputDialog("What is or number: ");
-	double calculo = Double.parseDouble(name);
-	
-	Retorno retorno = new Retorno(calculo);
-	
-	String nume = JOptionPane.showInputDialog("Numero para multiplicar");
-	double valor = Double.parseDouble(nume);	
-	retorno.contMais(valor);
-	String mens = String.format("%.2f", retorno.getCalculo());	
-	
-	JOptionPane.showMessageDialog(null, mens );
-	
-} catch (Exception e) {
-	JOptionPane.showMessageDialog(null, "Não deu certo!");
-	
-}
+		try {
+
+			String name = JOptionPane.showInputDialog("What is or number: ");
+			double calculo = Double.parseDouble(name);
+
+			Retorno retorno = new Retorno(calculo);
+
+			String nume = JOptionPane.showInputDialog("Numero para multiplicar");
+			double valor = Double.parseDouble(nume);
+			retorno.contMais(valor);
+			String mens = String.format("%.2f", retorno.getCalculo());
+
+			JOptionPane.showMessageDialog(null, mens);
+
+		}
+
+		catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Não deu certo!");
+
+		}
 
 	}
 
